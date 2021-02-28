4b063b2b8c8db6e1161fd8a1eacdc316b3a696b8
OmarIT555 <osalihbegovic@ggc.edu>
2021-02-27 20:18:00 -0500

    Created a way to delete tasks; Nga assisted

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,6 +3,8 @@ import 'package:next_step_planning/task_details_screen.dart';
 
 // This this the home screen. It contains all of the tasks the user have created.
 class MyHomePage extends StatefulWidget {
+  //MyHomePage({Key key}) : super(key: key);
+
   @override
   State<StatefulWidget> createState() {
     return _MyHomePageState();
@@ -12,6 +14,15 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int count = 0;
   int botNavBarIndex = 0;
+  List tasks = List<String>.generate(0, (index) => null);
+
+  List taskAssign() {
+    tasks = List<String>.generate(count, (index) => "Task " + (index + 1).toString());
+    return tasks;
+  }
+
+
+
 
   // Widget currentScreen = listViewBuilder(0);
 
@@ -19,6 +30,7 @@ class _MyHomePageState extends State<MyHomePage> {
   void _incrementCounter() {
     setState(() {
       count++;
+      tasks = taskAssign();
     });
   }
 
@@ -53,12 +65,29 @@ class _MyHomePageState extends State<MyHomePage> {
   // Creates tasks
   ListView listViewBuilder(int count) {
     return ListView.builder(
-      itemCount: count,
-      itemBuilder: (_, index) {
-        return ListTile(
-          title: Text("Task " + (index + 1).toString()),
-          subtitle: Text("Task is due by (insert date here)"),
-          leading: Icon(Icons.assignment_outlined),
+      itemCount: tasks.length,
+      itemBuilder: (context, index) {
+        final task = tasks[index];
+        return Dismissible(
+          // Each Dismissible must contain a Key. Keys allow Flutter to
+          // uniquely identify widgets.
+          key: Key(task),
+          // Provide a function that tells the app
+          // what to do after an item has been swiped away.
+          onDismissed: (direction) {
+            // Remove the item from the data source.
+            setState(() {
+              tasks.removeAt(index);
+            });
+
+            // Show a snackbar. This snackbar could also contain "Undo" actions.
+            Scaffold
+                .of(context)
+                .showSnackBar(SnackBar(content: Text("$tasks dismissed")));
+          },
+          child: ListTile(title: Text("Task " + (index + 1).toString()),
+              subtitle: Text("Task is due by (insert date here)"),
+            leading: Icon(Icons.assignment_outlined)),
         );
       },
     );
