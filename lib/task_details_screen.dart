2a6f14250681cfd5cd6227471164c8e100e4b4c1
Nga Tran <ngatran5225@gmail.com>
2021-03-10 11:40:04 -0500

    Fixed button color and overlapping backbutton on taskDetails screen

--- lib/task_details_screen.dart
+++ lib/task_details_screen.dart
@@ -8,68 +8,79 @@ import 'theme.dart';
 class TaskDetails extends StatelessWidget {
   final _textController = TextEditingController();
   String taskName;
+
   @override
   Widget build(BuildContext context) {
     ThemeChanger _themeChanger = Provider.of<ThemeChanger>(context);
     return Scaffold(
-      appBar: AppBar(
-          elevation: 0,
-          leadingWidth: 5,
-          title: Text(
-            "Task Details",
-            style: TextStyle(),
-          ),
-          actions: <Widget>[
-            Container(
-              margin: const EdgeInsets.only(
-                  top: 15.0, bottom: 15.0, right: 5.0, left: 5.0),
-              child: ElevatedButton(
-                child: Text("Save"),
-                onPressed: () {
-                  addTask(context);
-                },
-              ),
+        appBar: AppBar(
+            elevation: 0,
+            leadingWidth: 5,
+            automaticallyImplyLeading: false,
+            title: Text(
+              "Task Details",
+              style: TextStyle(),
             ),
-            Container(
-              margin: const EdgeInsets.only(
-                  top: 15.0, bottom: 15.0, right: 5.0, left: 5.0),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.grey,
+            actions: <Widget>[
+              Container(
+                margin: const EdgeInsets.only(
+                    top: 15.0, bottom: 15.0, right: 5.0, left: 5.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.green,
+                  ),
+                  child: Text("Save"),
+                  onPressed: () {
+                    addTask(context);
+                  },
                 ),
-                child: Text("Cancel"),
-                onPressed: () {
-                  print("Cancel button was clicked");
-                },
               ),
-            ),
-          ]),
-      body: Center(
-
-
-         child: Column(
-           children: <Widget>[
-             TextButton(
-                 child: Text('Light Theme'),
-                 onPressed: () => _themeChanger.setTheme(ThemeData.light().copyWith(
-                   accentColor: Colors.green,
-                   primaryColor: Colors.green,
-                 ))),
-             TextButton(
-                 child: Text('Dark Theme'),
-                 onPressed: () => _themeChanger.setTheme(ThemeData.dark().copyWith(
-                     accentColor: Colors.green,
-                     primaryColor: Colors.green))),
-             TextField(
-               onChanged: (name){
-                 taskName = name;
-               },
-               controller: _textController,)
-           ],
-    ),
-    )
-    );
-      //),
+              Container(
+                margin: const EdgeInsets.only(
+                    top: 15.0, bottom: 15.0, right: 5.0, left: 5.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.grey,
+                  ),
+                  child: Text("Cancel"),
+                  onPressed: () {
+                    print("Cancel button was clicked");
+                  },
+                ),
+              ),
+            ]),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              TextButton(
+                  style: TextButton.styleFrom(
+                    primary: Colors.green,
+                  ),
+                  child: Text('Light Theme'),
+                  onPressed: () =>
+                      _themeChanger.setTheme(ThemeData.light().copyWith(
+                        accentColor: Colors.green,
+                        primaryColor: Colors.green,
+                      ))),
+              TextButton(
+                  style: TextButton.styleFrom(
+                    primary: Colors.green,
+                  ),
+                  child: Text('Dark Theme'),
+                  onPressed: () => _themeChanger.setTheme(ThemeData.dark()
+                      .copyWith(
+                          accentColor: Colors.green,
+                          primaryColor: Colors.green))),
+              TextField(
+                onChanged: (name) {
+                  taskName = name;
+                },
+                controller: _textController,
+              )
+            ],
+          ),
+        ));
+    //),
     //);
   }
 
@@ -77,6 +88,6 @@ class TaskDetails extends StatelessWidget {
   void addTask(BuildContext context) {
     // Navigator.pop(context);
     Navigator.of(context).push(MaterialPageRoute(
-        builder: (context) => MyHomePage(taskName : taskName)));
+        builder: (context) => MyHomePage(taskName: taskName)));
   }
 }
